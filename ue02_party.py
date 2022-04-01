c2e56f7707b275a94875271774a289d1de2d073b
szapacss21 <102793462+szapacss21@users.noreply.github.com>
2022-04-01 11:31:01 +0200

    Add files via upload

---
+++ ue02_party.py
@@ -0,0 +1,18 @@
+
+wetter = input("Bitte 'Regen' oder 'Sonne' eingeben: ")
+wetter=wetter.lower()
+
+
+if wetter == "regen":
+    print("Keller")
+elif wetter == "sonne":
+    print("Terasse")
+else:
+    print("Bitte geben Sie das Wetter korrekt ein")
+    
+
+
+
+
+
+
